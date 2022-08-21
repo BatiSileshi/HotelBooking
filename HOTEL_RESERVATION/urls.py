a3be3ac56ced4b3cbba1e076957526ccc099d6ab
jiosaa <batisileshi11@gmail.com>
2022-08-21 07:39:47 -0400

    first commit

--- HOTEL_RESERVATION/urls.py
+++ HOTEL_RESERVATION/urls.py
@@ -4,7 +4,8 @@ from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static 
 from django.conf import settings
-
+from django.views.static import serve
+from django.urls import re_path
 
 
 urlpatterns = [
@@ -13,7 +14,9 @@ urlpatterns = [
     # path('hotel/', include('Hotel.urls')),
     path('sy-admin/', include('Systemadmin.urls')),
     path('hotel-admin/', include('Hoteladmin.urls')),
-    
+
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),  
   
 ]
 
