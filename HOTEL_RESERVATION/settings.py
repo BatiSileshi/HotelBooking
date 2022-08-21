c23090e2266f5b408aa747662ccb19ac437656dd
jiosaa <batisileshi11@gmail.com>
2022-08-21 10:20:25 -0400

    root file edited

--- HOTEL_RESERVATION/settings.py
+++ HOTEL_RESERVATION/settings.py
@@ -144,15 +144,18 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+
 MEDIA_URL='/images/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
   
-]
+# ]
 
 MEDIA_ROOT=os.path.join(BASE_DIR, 'static/images')
 
-STATIC_ROOT= BASE_DIR / 'static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
