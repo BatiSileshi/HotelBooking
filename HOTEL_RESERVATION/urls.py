8c6aea62d682f2037811d61d170e1ad0f7c0a6f6
jiosaa <batisileshi11@gmail.com>
2022-08-21 08:58:01 -0400

    runtime txt

--- HOTEL_RESERVATION/urls.py
+++ HOTEL_RESERVATION/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),  
  
-  
+ 
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
